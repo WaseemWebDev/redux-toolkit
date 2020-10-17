c6eb460fe2bb6252cfbf85c2cbceecdbc434f83b
WaseemWebDev <waseemyousafzai520@gmail.com>
2020-10-17 06:43:55 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Counter from './components/Counter';
+
+function App() {
+  return (
+    <div>
+      Hello App Component
+      <Counter />
+      
+    </div>
+
+  );
+}
+
+export default App;
